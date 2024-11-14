e6157b25345bd8974d77002182ec8eef8e8ba17b
Polawat Huang <polawathuang@gmail.com>
2024-11-14 09:52:43 +0700

    Fix bug about deployment

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cyA17qsCvaLo41iZL17m3RJasPuvOec5MiFoZj1MGVk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Is it fruits or vegetable?",
+  description: "Let's start learning about the basic coding of HTML + CSS in Next.js project, what's named 'Is it fruits or vegetables?'. Plant foods with seeds, such as cucumbers, zucchini, eggplant, tomatoes, strawberries, and mangos, are all fruits. Foods without seeds, such as broccoli, cauliflower, kale, lettuce, and carrots, are vegetables.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
